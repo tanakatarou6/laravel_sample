28389f63472c89ac8f6faac422fc139b13d4f5ea
tanakatarou6 <tanakatarou590@gmail.com>
2023-01-28 01:29:10 +0900

    problem

--- app/Problem.php
+++ app/Problem.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Problem extends Model
 {
     // fillableでロックをかける
-    protected $fillable = ['drills_id', 'problem_0', 'problem_1', 'problem_2', 'problem_3', 'problem_4', 'problem_5', 'problem_6', 'problem_7', 'problem_8', 'problem_9'];
+    protected $fillable = ['drill_id', 'problem_0', 'problem_1', 'problem_2', 'problem_3', 'problem_4', 'problem_5', 'problem_6', 'problem_7', 'problem_8', 'problem_9'];
 
     // リレーションを張る
     public function drill()
